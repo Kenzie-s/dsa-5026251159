c364f0c2a29321cb1a53bdfe1dcb9ffb134ad655
Yehezkiel Christian Mc Kenzie Sihombing <mckenzie.sihombing@gmail.com>
2026-09-20 21:56:44 +0700

    undo test

--- src/lw01/prelab/Main.java
+++ src/lw01/prelab/Main.java
@@ -12,8 +12,6 @@ public class Main {
     public static void main(String[] args) {
         File file = findJobsFile();
         Scanner scanner = null;
-
-        System.out.println("test");
         try {
             if (file != null && file.exists()) {
                 scanner = new Scanner(file);
